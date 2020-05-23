81da0577be5247da99bd1ecfb4d6c19e781969d4
Jens Paetzold <paetzold@multichannelsystems.com>
2020-05-23 23:38:52 +0200

    added create.bat script with my used commands

---
+++ Freizeitkarte-Entwicklung/create.bat
@@ -0,0 +1,21 @@
+perl mt.pl fetch_osm BADEN-WUERTTEMBERG
+perl mt.pl fetch_ele BADEN-WUERTTEMBERG
+perl mt.pl join BADEN-WUERTTEMBERG
+perl mt.pl split BADEN-WUERTTEMBERG
+#perl mt.pl bml BADEN-WUERTTEMBERG
+perl mt.pl bml BADEN-WUERTTEMBERG DBICYCLEROUTE
+perl mt.pl bml DEU+
+# Karten im Ordner EUROPE löschen
+perl mt.pl fetch_osm EUROPE
+#perl mt.pl extract_osm DEU+
+#perl mt.pl join DEU+
+#perl mt.pl split DEU+
+perl mt.pl pmd DEU+
+perl mt.pl bml DEU+ DBICYCLEROUTE
+perl mt.pl pmd FRA+
+#perl mt.pl bml FRA+
+perl mt.pl bml FRA+ DBICYCLEROUTE
+perl mt.pl pmd CHE+
+perl mt.pl bml CHE+ DBICYCLEROUTE
+perl mt.pl pmd AUT+
+perl mt.pl bml AUT+ DBICYCLEROUTE
